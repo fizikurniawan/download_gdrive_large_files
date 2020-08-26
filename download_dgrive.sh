d1fa3ffcb71a74e8a326078af6a7e6a2e7fbf886
Fizi Kurniawan <fizikurniawan@gmail.com>
2020-08-26 11:12:48 +0700

    Create download_dgrive.sh

---
+++ download_dgrive.sh
@@ -0,0 +1,5 @@
+fileId="your-file-id"
+fileName="your-file-name"
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName} 
